8996b90ad3ebbcdee2547fc57a5345473b2e7914
bodrul2112 <bodrul2112@gmail.com>
2013-04-30 14:05:08 +0100

    bog standard socket io example

--- server/app.js
+++ server/app.js
@@ -1,3 +1,6 @@
+
+
+/*
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
@@ -5,7 +8,7 @@ var app = require('http').createServer(handler)
 app.listen(3000);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
+  fs.readFile(__dirname + '/client/index.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
@@ -22,4 +25,22 @@ io.sockets.on('connection', function (socket) {
   socket.on('my other event', function (data) {
     console.log(data);
   });
+});
+*/
+
+var app = require('express')()
+    , server = require('http').createServer(app)
+    , io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/client/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
