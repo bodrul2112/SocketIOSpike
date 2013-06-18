4f4a4d6dddb3bc4312a3851c91c26bc1890152c5
Bodrul Alam <bodrul2112@gmail.com>
2013-06-18 23:46:42 +0100

    loading socket io as a client lib
    not super cool but oh well

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 
 
-/*
+
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
@@ -26,21 +26,4 @@ io.sockets.on('connection', function (socket) {
     console.log(data);
   });
 });
-*/
 
-var app = require('express')()
-    , server = require('http').createServer(app)
-    , io = require('socket.io').listen(server);
-
-server.listen(3000);
-
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/client/index.html');
-});
-
-io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
-});
